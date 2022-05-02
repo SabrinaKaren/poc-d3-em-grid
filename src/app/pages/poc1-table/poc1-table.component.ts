262516061443da0339411daff483ecd4286a4d71
SabrinaKaren <sabrinaa.karen@hotmail.com>
2022-05-02 15:04:21 -0300

    POC 1: "Ver mais"

--- src/app/pages/poc1-table/poc1-table.component.ts
+++ src/app/pages/poc1-table/poc1-table.component.ts
@@ -13,20 +13,20 @@ export class Poc1TableComponent implements OnInit {
   readonly URL_4FLAGS: string = '/assets/flag-score/4-flags.png';
   readonly URL_5FLAGS: string = '/assets/flag-score/5-flags.png';
 
-  columnsIdentificator: string[] = ['cgf', 'notaGeral', 'historico', 'pesoNaNotaGeral'];
+  columnsIdentificator: string[] = ['cgf', 'notaGeral', 'historico', 'pesoNaNotaGeral', 'empty'];
   tableData: PeriodicElement[];
 
   constructor() {
     /* dados mockados */
     this.tableData = [
-      { id: '1', cgf: 'COMÉRCIO ATACAREJO RJ - 500653', notaGeral: 4, historico: '', pesoNaNotaGeral: 2.5 },
-      { id: '2', cgf: 'COMÉRCIO ATACAREJO RS - 432764', notaGeral: 4, historico: '', pesoNaNotaGeral: 75 },
-      { id: '3', cgf: 'COMÉRCIO ATACAREJO ES - 842995', notaGeral: 5, historico: '', pesoNaNotaGeral: 65.1 },
-      { id: '4', cgf: 'COMÉRCIO ATACAREJO DF - 300510', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.4 },
-      { id: '5', cgf: 'COMÉRCIO ATACAREJO GO - 667310', notaGeral: 5, historico: '', pesoNaNotaGeral: 47.4 },
-      { id: '6', cgf: 'COMÉRCIO ATACAREJO AM - 728860', notaGeral: 2, historico: '', pesoNaNotaGeral: 23 },
-      { id: '7', cgf: 'COMÉRCIO ATACAREJO RJ - 827774', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.9 },
-      { id: '8', cgf: 'COMÉRCIO ATACAREJO SP - 120198', notaGeral: 5, historico: '', pesoNaNotaGeral: 29 }
+      { id: '1', cgf: 'COMÉRCIO ATACAREJO RJ - 500653', notaGeral: 4, historico: '', pesoNaNotaGeral: 2.5, empty: '' },
+      { id: '2', cgf: 'COMÉRCIO ATACAREJO RS - 432764', notaGeral: 4, historico: '', pesoNaNotaGeral: 75, empty: '' },
+      { id: '3', cgf: 'COMÉRCIO ATACAREJO ES - 842995', notaGeral: 5, historico: '', pesoNaNotaGeral: 65.1, empty: '' },
+      { id: '4', cgf: 'COMÉRCIO ATACAREJO DF - 300510', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.4, empty: '' },
+      { id: '5', cgf: 'COMÉRCIO ATACAREJO GO - 667310', notaGeral: 5, historico: '', pesoNaNotaGeral: 47.4, empty: '' },
+      { id: '6', cgf: 'COMÉRCIO ATACAREJO AM - 728860', notaGeral: 2, historico: '', pesoNaNotaGeral: 23, empty: '' },
+      { id: '7', cgf: 'COMÉRCIO ATACAREJO RJ - 827774', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.9, empty: '' },
+      { id: '8', cgf: 'COMÉRCIO ATACAREJO SP - 120198', notaGeral: 5, historico: '', pesoNaNotaGeral: 29, empty: '' }
     ];
   }
 
@@ -58,4 +58,5 @@ export interface PeriodicElement {
   notaGeral: number;
   historico: any;
   pesoNaNotaGeral: number;
+  empty: any;
 }
