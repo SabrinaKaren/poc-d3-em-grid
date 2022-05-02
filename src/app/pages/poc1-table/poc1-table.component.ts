2e304e08b2df5ffcd4fd1fb50ce09e0f2811a9af
SabrinaKaren <sabrinaa.karen@hotmail.com>
2022-05-02 14:50:34 -0300

    POC 1: inclusão de flags para notaGeral

--- src/app/pages/poc1-table/poc1-table.component.ts
+++ src/app/pages/poc1-table/poc1-table.component.ts
@@ -7,23 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Poc1TableComponent implements OnInit {
 
+  readonly URL_1FLAG: string = '/assets/flag-score/1-flag.png';
+  readonly URL_2FLAGS: string = '/assets/flag-score/2-flags.png';
+  readonly URL_3FLAGS: string = '/assets/flag-score/3-flags.png';
+  readonly URL_4FLAGS: string = '/assets/flag-score/4-flags.png';
+  readonly URL_5FLAGS: string = '/assets/flag-score/5-flags.png';
+
   columnsIdentificator: string[] = ['cgf', 'notaGeral', 'historico', 'pesoNaNotaGeral'];
   tableData: PeriodicElement[];
 
   constructor() {
+    /* dados mockados */
     this.tableData = [
       { id: '1', cgf: 'COMÉRCIO ATACAREJO RJ - 500653', notaGeral: 4, historico: '', pesoNaNotaGeral: 2.5 },
-      { id: '2', cgf: 'COMÉRCIO ATACAREJO RS - 432764', notaGeral: 1, historico: '', pesoNaNotaGeral: 75 },
+      { id: '2', cgf: 'COMÉRCIO ATACAREJO RS - 432764', notaGeral: 4, historico: '', pesoNaNotaGeral: 75 },
       { id: '3', cgf: 'COMÉRCIO ATACAREJO ES - 842995', notaGeral: 5, historico: '', pesoNaNotaGeral: 65.1 },
       { id: '4', cgf: 'COMÉRCIO ATACAREJO DF - 300510', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.4 },
       { id: '5', cgf: 'COMÉRCIO ATACAREJO GO - 667310', notaGeral: 5, historico: '', pesoNaNotaGeral: 47.4 },
       { id: '6', cgf: 'COMÉRCIO ATACAREJO AM - 728860', notaGeral: 2, historico: '', pesoNaNotaGeral: 23 },
       { id: '7', cgf: 'COMÉRCIO ATACAREJO RJ - 827774', notaGeral: 3, historico: '', pesoNaNotaGeral: 2.9 },
-      { id: '8', cgf: 'COMÉRCIO ATACAREJO SP - 120198', notaGeral: 1, historico: '', pesoNaNotaGeral: 29 }
+      { id: '8', cgf: 'COMÉRCIO ATACAREJO SP - 120198', notaGeral: 5, historico: '', pesoNaNotaGeral: 29 }
     ];
   }
 
   ngOnInit(): void {
+  }
+
+  getUrlFlagByScore(score: number): string {
+    switch(score) {
+      case 1:
+        return this.URL_1FLAG;
+      case 2:
+        return this.URL_2FLAGS;
+      case 3:
+        return this.URL_3FLAGS;
+      case 4:
+        return this.URL_4FLAGS;
+      case 5:
+        return this.URL_5FLAGS;
+      default:
+        return this.URL_1FLAG;
+    }
   }
   
 }
